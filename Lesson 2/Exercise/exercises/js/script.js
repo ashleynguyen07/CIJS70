910421de544f7a618dda26c8473b2321a0d61ac7
Nguyen Thi Tra My <ameenguyen.07@gmail.com>
2021-12-24 14:18:41 +0700

    exercises

--- Lesson 2/Exercise/exercises/js/script.js
+++ Lesson 2/Exercise/exercises/js/script.js
@@ -25,3 +25,20 @@ document.getElementById("myID").addEventListener("mouseover", function() {
 document.getElementById("myID").addEventListener("mouseout", function() {
     document.getElementById("myID").style.background = "red";
 })
+
+// Exercise 3
+
+let makeTriangle = () => {
+    let toPrint = "";
+    let rowCount = 5;
+  
+    for (let row = 1; row <= rowCount; row++) {
+      for (let numOne = 1; numOne <= row; numOne++)
+        toPrint += "1";
+  
+      toPrint += "<br>";
+    }
+    document.getElementById("outputNumber").innerHTML = toPrint
+  }
+  
+  makeTriangle();
